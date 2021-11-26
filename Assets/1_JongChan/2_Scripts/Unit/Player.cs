45fbd13f549afcb6f7a50827eb9e5eb15bdc8d08
longblandhair <chan031120@gmail.com>
2021-11-26 14:18:25 +0900

    플레이어 + 아웃라인 섀이더

--- Assets/1_JongChan/2_Scripts/Unit/Player.cs
+++ Assets/1_JongChan/2_Scripts/Unit/Player.cs
@@ -4,71 +4,84 @@ using System.Collections.Generic;
 using System.Net.NetworkInformation;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Player : Unit
 {
-    private float moveSpeed;
-    private float jumpPower;
-    private int isjump;
-    
+    [Header("Player")] [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private int curJump;
+    [SerializeField] private int maxJump;
+    [SerializeField] private bool isDash;
+    [SerializeField] private float AttackRange;
+
+    private float dashTimer = 0;
+    private float dashInterval = 1;
+    private Collider2D weaponcollider;
+
     protected override void Start()
     {
         base.Start();
-    }
-
-    private void Update()
-    {
-        Move();
+        weaponcollider = transform.Find("Weapon").GetComponent<Collider2D>();
     }
     
-    void Move()
+    private void Update()
+    {
+        dashTimer += Time.deltaTime;
+
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        Idle(); 
+        Move();
+        Dash();
+        Attack();
+    }
+
+    private void Idle()
+    {
+        //Idle 애니메이션 추가
+    }
+
+    private void Move()
     {
         float Horizontal = Input.GetAxisRaw("Horizontal");
+
+        rigidbody2D.AddForce(Vector2.right * Horizontal, ForceMode2D.Impulse);
+
+        if (rigidbody2D.velocity.x > moveSpeed)
+            rigidbody2D.velocity = new Vector2(moveSpeed, rigidbody2D.velocity.y);
+        else if (rigidbody2D.velocity.x < moveSpeed * (-1))
+            rigidbody2D.velocity = new Vector2(moveSpeed * (-1), rigidbody2D.velocity.y);
 
         if (Input.GetButtonUp("Horizontal"))
         {
             rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.normalized.x * 0f, rigidbody2D.velocity.y);
         }
-
-        rigidbody2D.AddForce(Vector2.right * Horizontal, ForceMode2D.Impulse);
-
-        if (rigidbody2D.velocity.x > moveSpeed)
-        {
-            rigidbody2D.velocity = new Vector2(moveSpeed, rigidbody2D.velocity.y);
-        }
-        else if (rigidbody2D.velocity.x < moveSpeed * (-1))
-        {
-            rigidbody2D.velocity = new Vector2(moveSpeed * (-1), rigidbody2D.velocity.y);
-        }
-
-        if (isjump >= 0)
+        
+        if (curJump >= 0)
         {
             if (Input.GetButtonDown("Jump"))
             {
-                if (isjump == 2)
-                {
-                    // anim.SetTrigger("isJump");
-                }
-
-                else if (isjump == 1)
+                if (curJump == 1)
                 {
                     // anim.SetTrigger("isDoubleJump");
                 }
                 rigidbody2D.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-                isjump--;
+                curJump--;
             }
         }
 
         if (Input.GetAxisRaw("Horizontal") < 0)
         {
-            transform.localScale = new Vector3(1, 1, 1);
-            // anim.SetBool("isRun", true);
+            spriteRenderer.flipX = true;
         }
 
         else if (Input.GetAxisRaw("Horizontal") > 0)
         {
-            transform.localScale = new Vector3(-1, 1, 1);
-            // anim.SetBool("isRun", true);
+            spriteRenderer.flipX = false;
         }
 
         else
@@ -77,10 +90,65 @@ public class Player : Unit
         }
     }
 
-    private void Skill1()
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            StartCoroutine(Co_Attack());
+        }
+    }
+
+    IEnumerator Co_Attack()
+    {
+        //공격 애니메이션 추가 할 곳
+        weaponcollider.enabled = true;
+        yield return YieldCache.WaitForSeconds(1f);
+        weaponcollider.enabled = false;
+        yield return null;
+    }
+
+    private void Dash()
+    {
+        if (Input.GetKeyDown(KeyCode.X) && dashTimer > dashInterval)
+        {
+            print("Dash");
+            StartCoroutine(Co_Dash());
+        }
+    }
+
+    IEnumerator Co_Dash()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            yield return YieldCache.WaitForSeconds(0.01f);
+            transform.position += new Vector3((spriteRenderer.flipX ? -moveSpeed : moveSpeed) * 10 * Time.deltaTime, 0, 0);
+        }
+    }
+
+    private void Skill_1()
+    {
+        StartCoroutine(Co_Skill_1());
+    }
+
+    IEnumerator Co_Skill_1()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            yield return YieldCache.WaitForSeconds(0.01f);
+            transform.position += new Vector3((spriteRenderer.flipX ? -moveSpeed : moveSpeed) * 10 * Time.deltaTime, 0, 0);
+        }
+    }
+
+    private void Skill_2()
     {
         
     }
-    
-    
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "Ground")
+        {
+            curJump = maxJump;
+        }
+    }
 }
---
+++ Assets/1_JongChan/4_Shader/Outline.shader
@@ -0,0 +1,79 @@
+Shader "Unlit/Outline"
+{
+    Properties
+    {
+        _MainTex ("Sprite Texture", 2D) = "white" {}
+        _OutlineColor ("OutlineColor", Color) = (1,1,1,1)
+        _OutlineSize ("OutlineSize", Range(0.0, 0.1)) = 0.1
+    }
+
+    SubShader
+    {
+        Tags
+        {
+            "Queue"="Transparent"
+        }
+
+        Blend SrcAlpha OneMinusSrcAlpha
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+         
+            #include "UnityCG.cginc"
+         
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+         
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+         
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            float4 _MainTex_TexelSize;
+         
+            fixed4 _OutlineColor;
+            float _OutlineSize;
+         
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+         
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed4 col = tex2D(_MainTex, i.uv);
+
+
+                if (_OutlineSize != 0)
+                {
+                    fixed leftPixel = tex2D(_MainTex, i.uv + float2(-_MainTex_TexelSize.x - _OutlineSize, 0)).a;
+                    fixed upPixel = tex2D(_MainTex, i.uv + float2(0, _MainTex_TexelSize.y + _OutlineSize)).a;
+                    fixed rightPixel = tex2D(_MainTex, i.uv + float2(_MainTex_TexelSize.x + _OutlineSize, 0)).a;
+                    fixed bottomPixel = tex2D(_MainTex, i.uv + float2(0, -_MainTex_TexelSize.y - _OutlineSize)).a;
+                    
+                    fixed outline = (1 - leftPixel * upPixel * rightPixel * bottomPixel) * col.a;
+                    
+                    return lerp(col, _OutlineColor, outline);
+                }
+                
+                else
+                {
+                    return col;
+                }
+            }
+            ENDCG
+        }
+    }
+}
